f604f57c321f30e4aaba9aa24ce766d787519de2
Victor Perri <113261904+VictorPerri@users.noreply.github.com>
2025-09-19 08:42:09 -0300

    atualização painel_funcionario.php

--- Website/Fatalvenom/app/views/painel_funcionario.php
+++ Website/Fatalvenom/app/views/painel_funcionario.php
@@ -1,9 +1,6 @@
 <?php
-// painel_funcionario.php
-
 session_start();
 
-// Check if user is logged in and is staff
 if (!isset($_SESSION['nome']) || $_SESSION['cargo'] !== 'Administrador') {
     header('Location: /login.php');
     exit;
